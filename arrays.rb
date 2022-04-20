8fded935b9220a6c9fdba5872d679711349d2482
Anna Marie Sterling <sterling.am@gmail.com>
2022-04-20 13:21:45 -0600

    Added comment in array

--- arrays.rb
+++ arrays.rb
@@ -3,3 +3,4 @@ clients = ["Hair", "Wax", "Makeup", "Retail"]
 appointments = [30, 60, 90, 75]
 cost = [75.00, 125.00, 350.00, 920.00]
 paid = [true, true, false, false]
+# .length for any of these arrays should return =>3
