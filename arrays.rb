b4f76e73fd049248ee93ba93bb62d30ca54a162d
Anna Marie Sterling <sterling.am@gmail.com>
2022-04-20 19:01:15 -0600

    Add array position explanation

--- arrays.rb
+++ arrays.rb
@@ -3,4 +3,5 @@ clients = ["Hair", "Wax", "Makeup", "Retail"]
 appointments = [30, 60, 90, 75]
 cost = [75.00, 125.00, 350.00, 920.00]
 paid = [true, true, false, false]
-# .length for any of these arrays should return =>3
+# .length for any of these arrays should return => 4 I changed from => 3 because it is a count, not a position is what I am gathering
+# clients[0] would return "Hair", cost[2] would return 350.00 
